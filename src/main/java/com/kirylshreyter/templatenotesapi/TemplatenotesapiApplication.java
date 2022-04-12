b5d748b22104a72f7dd5f49f711a24f509cce19c
Kiryl Shreyter <kirylshreyter@gmail.com>
2022-04-12 20:26:42 +0200

    initial commit

---
+++ src/main/java/com/kirylshreyter/templatenotesapi/TemplatenotesapiApplication.java
@@ -0,0 +1,13 @@
+package com.kirylshreyter.templatenotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatenotesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatenotesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirylshreyter/templatenotesapi/TemplatenotesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirylshreyter.templatenotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplatenotesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
